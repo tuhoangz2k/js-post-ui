3068220919c4fbe4d8c4a8f8ae9eee287d2eaa4e
Tu Hoang <tuhoangz2k@gmail.com>
2022-07-20 18:19:17 +0700

    init commit

---
+++ vite.config.js
@@ -0,0 +1,16 @@
+// vite.config.js
+import { resolve } from 'path'
+import { defineConfig } from 'vite'
+
+export default defineConfig({
+  build: {
+    rollupOptions: {
+      input: {
+        main: resolve(__dirname, 'index.html'),
+        postDetail: resolve(__dirname, 'post-detail.html'),
+        addEditPost: resolve(__dirname, 'add-edit-post.html')
+
+      }
+    }
+  }
+})
